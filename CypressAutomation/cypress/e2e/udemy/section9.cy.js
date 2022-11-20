1c35947ecd3f01de11195818c2aef89b55206e88
Ben Bowen <bcbowen@gmail.com>
2022-11-20 18:31:14 -0500

    Add pageobjects to model page elements

--- CypressAutomation/cypress/e2e/udemy/section9.cy.js
+++ CypressAutomation/cypress/e2e/udemy/section9.cy.js
@@ -1,36 +1,36 @@
 /// <reference types="Cypress" />
 
+import HomePage from './pageObjects/HomePage';
+
 describe('Cypress Framework, fixtures and custom commands', function () {
   beforeEach(function () {
     cy.fixture('example').then(function (data) {
       this.data = data;
+      this.homePage = new HomePage();
     });
   });
 
   it('45: Load test data from fixture', function () {
     cy.visit('https://rahulshettyacademy.com/angularpractice/');
-    cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
-    cy.get('select').select(this.data.gender);
+    this.homePage.getEditBox().type(this.data.name);
+    this.homePage.getGender().select(this.data.gender);
   });
 
   it('46: Validate attribute properties and behavior', function () {
     cy.visit('https://rahulshettyacademy.com/angularpractice/');
-    cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
-    cy.get('input[name="name"]:nth-child(1)').should(
-      'have.value',
-      this.data.name
-    );
-    cy.get('input[name="name"]:nth-child(2)').should(
-      'have.attr',
-      'minlength',
-      '2'
-    );
-    cy.get('#inlineRadio3').should('be.disabled');
+    this.homePage.getEditBox().type(this.data.name);
+    this.homePage.getTwoWayDataBinding().should('have.value', this.data.name);
+    this.homePage.getEditBox().should('have.attr', 'minlength', '2');
+    this.homePage.getEntrepreneurRadio().should('be.disabled');
   });
 
   it('47: Customized commands for code reuse', function () {
     cy.visit('https://rahulshettyacademy.com/angularpractice/shop');
     // customized command in support.command.js
-    cy.selectProduct('Blackberry');
+    // pause method to break in debug mode:
+    // cy.pause();
+    this.data.productName.forEach(function (name) {
+      cy.selectProduct(name);
+    });
   });
 });
