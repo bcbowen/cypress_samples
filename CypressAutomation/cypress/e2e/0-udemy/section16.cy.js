39a6de9a1ce3ff0673f9d906d3ed5173aa9c38aa
Ben Bowen <bcbowen@gmail.com>
2022-11-29 00:14:06 -0500

    Add mock request demo

--- CypressAutomation/cypress/e2e/0-udemy/section16.cy.js
+++ CypressAutomation/cypress/e2e/0-udemy/section16.cy.js
@@ -1,7 +1,7 @@
 /// <reference Types="Cypress" />
 
 describe('Mocking Http requests/responses (XHR testing)', function () {
-  it('77: Mock response test', function () {
+  it('77-79: Mock response test', function () {
     cy.visit('https://rahulshettyacademy.com/angularAppdemo');
     cy.intercept(
       {
@@ -20,7 +20,25 @@ describe('Mocking Http requests/responses (XHR testing)', function () {
       }
     ).as('bookretrievals');
     cy.get("button[class='btn btn-primary']").click();
-    cy.wait('@bookretrievals');
+    cy.wait('@bookretrievals').should(({ request, response }) => {
+      cy.get('tr').should('have.length', response.body.length + 1);
+
+    });
     cy.get('p').should('have.text', 'Oops only 1 Book available');
   });
+
+  it('80: Mock request test', function () {
+    cy.visit('https://rahulshettyacademy.com/angularAppdemo');
+    cy.intercept('GET', 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty',
+      (req) => {
+        req.url = 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=malhotra';
+        req.continue((res) => {
+          expect(res.statusCode).to.equal(403);
+        });
+      }).as('testUrl');
+    cy.get("button[class='btn btn-primary']").click();
+    cy.wait('@testUrl');
+    //cy.get('p').should('have.text', 'Oops only 1 Book available');
+  });
+
 });
