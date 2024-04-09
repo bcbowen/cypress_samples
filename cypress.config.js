873df6d92d132cdb9746f8d5c763243d84705560
Ben Bowen <bcbowen@gmail.com>
2024-04-08 21:54:30 -0400

    Sample apps

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false, 
+  defaultCommandTimeout: 8000, 
+  pageLoadTimeout: 30000, 
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      //return require('./cypress/')(); 
     },
     specPattern: 'cypress/integration/examples/'
   },
