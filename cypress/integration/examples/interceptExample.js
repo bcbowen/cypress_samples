7b0f0724068007e88f0bf2cc0b48d8b859d3a609
Ben Bowen <bcbowen@gmail.com>
2024-04-08 22:42:48 -0400

    Add intercept example

---
+++ cypress/integration/examples/interceptExample.js
@@ -0,0 +1,21 @@
+describe('Example of intercepting http calls for api testing', () => {
+    
+    it('Correct message displayed when only one book is returned', () => {
+        cy.visit("https://rahulshettyacademy.com/angularAppdemo/"); 
+        cy.intercept({
+            method: 'GET', 
+            url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+        }, {
+            statusCode: 200, 
+            body: [{
+                "book_name": "RestAssured with Java",
+                "isbn": "yada232",
+                "aisle": "2301"
+            }]
+        }).as('booksReturned'); 
+        cy.get("button[class='btn btn-primary']").click(); 
+        cy.wait('@booksReturned'); 
+        cy.get('p').should('have.text', 'Oops only 1 Book available');
+    }); 
+    
+}); 
